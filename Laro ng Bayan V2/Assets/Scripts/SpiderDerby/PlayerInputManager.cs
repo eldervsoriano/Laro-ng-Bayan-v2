549e6dfb7c82e1869c5a2e21f24324617af3b2af
eldervsoriano <eldervsoriano@gmail.com>
2025-04-29 00:07:01 +0800

    Spider Derby finalization

--- Laro ng Bayan V2/Assets/Scripts/SpiderDerby/PlayerInputManager.cs
+++ Laro ng Bayan V2/Assets/Scripts/SpiderDerby/PlayerInputManager.cs
@@ -1,3 +1,174 @@
+//using UnityEngine;
+//using UnityEngine.UI;  // For UI Image handling
+//using TMPro;  // For TextMeshPro functionality (UI)
+
+//public class PlayerInputManager : MonoBehaviour
+//{
+//    public int playerNumber = 1;  // 1 for Player 1, 2 for Player 2
+//    public Image rockImage;       // Image for Rock
+//    public Image paperImage;      // Image for Paper
+//    public Image scissorsImage;   // Image for Scissors
+//    public Image rockImagePlaceholder; // Placeholder for Rock
+//    public Image paperImagePlaceholder; // Placeholder for Paper
+//    public Image scissorsImagePlaceholder; // Placeholder for Scissors
+
+//    // Timer UI for showing countdown
+//    public TextMeshProUGUI timerText;
+
+//    // Player 1 keys (Q, W, E) and Player 2 keys (Keypad1, Keypad2, Keypad3)
+//    public KeyCode keyToSelectRock = KeyCode.Q;
+//    public KeyCode keyToSelectPaper = KeyCode.W;
+//    public KeyCode keyToSelectScissors = KeyCode.E;
+
+//    public KeyCode keyToSelectRockPlayer2 = KeyCode.Keypad1;
+//    public KeyCode keyToSelectPaperPlayer2 = KeyCode.Keypad2;
+//    public KeyCode keyToSelectScissorsPlayer2 = KeyCode.Keypad3;
+
+//    // Single variable to store player's choice (1=Rock, 2=Paper, 3=Scissors)
+//    private int playerChoice = 0;  // Tracks the player's current choice (1 for Rock, 2 for Paper, 3 for Scissors)
+//    private bool isSelecting = true;  // Flag to allow selection of the player's action
+
+//    private float timeToChoose = 5f;  // Time limit for making a choice (in seconds)
+//    private float timer;  // Timer to show countdown
+
+//    void Start()
+//    {
+//        // Initially, hide all the images (choices are hidden at the start)
+//        rockImage.gameObject.SetActive(false);
+//        paperImage.gameObject.SetActive(false);
+//        scissorsImage.gameObject.SetActive(false);
+
+//        // Initially show placeholders for selection
+//        rockImagePlaceholder.gameObject.SetActive(true);
+//        paperImagePlaceholder.gameObject.SetActive(true);
+//        scissorsImagePlaceholder.gameObject.SetActive(true);
+
+//        timer = timeToChoose;  // Set timer
+
+//        // Start a countdown for selection time
+//        Invoke("StopSelecting", timeToChoose);
+//    }
+
+//    void Update()
+//    {
+//        if (isSelecting)
+//        {
+//            // Handle player input based on the player number
+//            if (playerNumber == 1)  // Player 1 input (Q, W, E for Rock, Paper, Scissors)
+//            {
+//                if (Input.GetKeyDown(keyToSelectRock))
+//                {
+//                    SetChoice(1);  // Rock
+//                }
+//                else if (Input.GetKeyDown(keyToSelectPaper))
+//                {
+//                    SetChoice(2);  // Paper
+//                }
+//                else if (Input.GetKeyDown(keyToSelectScissors))
+//                {
+//                    SetChoice(3);  // Scissors
+//                }
+//            }
+//            else if (playerNumber == 2)  // Player 2 input (Keypad1, Keypad2, Keypad3 for Rock, Paper, Scissors)
+//            {
+//                if (Input.GetKeyDown(keyToSelectRockPlayer2))
+//                {
+//                    SetChoice(1);  // Rock
+//                }
+//                else if (Input.GetKeyDown(keyToSelectPaperPlayer2))
+//                {
+//                    SetChoice(2);  // Paper
+//                }
+//                else if (Input.GetKeyDown(keyToSelectScissorsPlayer2))
+//                {
+//                    SetChoice(3);  // Scissors
+//                }
+//            }
+//        }
+
+//        // Update the timer text during the selection phase
+//        if (isSelecting)
+//        {
+//            timer -= Time.deltaTime;
+//            timerText.text = Mathf.Ceil(timer).ToString();  // Update the UI with remaining time
+//        }
+//    }
+
+//    // Method to handle player's choice
+//    // Make this method public so SpiderGameManager can access it
+//    public void SetChoice(int choice)
+//    {
+//        playerChoice = choice;
+
+//        // Show the corresponding image based on the choice
+//        switch (choice)
+//        {
+//            case 1:
+//                rockImage.gameObject.SetActive(true);
+//                break;
+//            case 2:
+//                paperImage.gameObject.SetActive(true);
+//                break;
+//            case 3:
+//                scissorsImage.gameObject.SetActive(true);
+//                break;
+//        }
+
+//        // Hide the images for the other selections (only one choice can be shown at a time)
+//        rockImage.gameObject.SetActive(choice == 1);
+//        paperImage.gameObject.SetActive(choice == 2);
+//        scissorsImage.gameObject.SetActive(choice == 3);
+//    }
+
+//    // Stop the player's ability to select after the time runs out
+//    public void StopSelecting()
+//    {
+//        isSelecting = false;
+
+//        // Hide the placeholders and reveal actual selections after time is up
+//        rockImage.gameObject.SetActive(false);
+//        paperImage.gameObject.SetActive(false);
+//        scissorsImage.gameObject.SetActive(false);
+
+//        // Hide placeholders
+//        rockImagePlaceholder.gameObject.SetActive(false);
+//        paperImagePlaceholder.gameObject.SetActive(false);
+//        scissorsImagePlaceholder.gameObject.SetActive(false);
+
+//        // Trigger the game logic to compare selections
+//        SpiderGameManager.Instance.CompareSelections();
+//    }
+
+//    // Getter method for playerChoice
+//    public int GetPlayerChoice()
+//    {
+//        return playerChoice;
+//    }
+
+//    // Reset the player's choices after a round ends
+//    public void ResetSelection()
+//    {
+//        // Reset the player's current selection (so they can pick again)
+//        playerChoice = 0;
+
+//        // Reset the UI to show placeholders and hide selections
+//        rockImage.gameObject.SetActive(false);
+//        paperImage.gameObject.SetActive(false);
+//        scissorsImage.gameObject.SetActive(false);
+
+//        rockImagePlaceholder.gameObject.SetActive(true);
+//        paperImagePlaceholder.gameObject.SetActive(true);
+//        scissorsImagePlaceholder.gameObject.SetActive(true);
+//    }
+
+//    // Method to allow player to start selecting again
+//    public void StartSelecting()
+//    {
+//        isSelecting = true;  // Re-enable selection
+//        timer = timeToChoose;  // Reset the timer for this new round
+//    }
+//}
+
 using UnityEngine;
 using UnityEngine.UI;  // For UI Image handling
 using TMPro;  // For TextMeshPro functionality (UI)
@@ -100,24 +271,17 @@ public class PlayerInputManager : MonoBehaviour
     {
         playerChoice = choice;
 
-        // Show the corresponding image based on the choice
-        switch (choice)
-        {
-            case 1:
-                rockImage.gameObject.SetActive(true);
-                break;
-            case 2:
-                paperImage.gameObject.SetActive(true);
-                break;
-            case 3:
-                scissorsImage.gameObject.SetActive(true);
-                break;
-        }
+        // During selection, don't show the choice yet
+        // We'll reveal it when time runs out
+    }
 
-        // Hide the images for the other selections (only one choice can be shown at a time)
-        rockImage.gameObject.SetActive(choice == 1);
-        paperImage.gameObject.SetActive(choice == 2);
-        scissorsImage.gameObject.SetActive(choice == 3);
+    // New method to reveal the player's choice after time runs out
+    public void RevealChoice()
+    {
+        // Show the corresponding image based on the choice
+        rockImage.gameObject.SetActive(playerChoice == 1);
+        paperImage.gameObject.SetActive(playerChoice == 2);
+        scissorsImage.gameObject.SetActive(playerChoice == 3);
     }
 
     // Stop the player's ability to select after the time runs out
@@ -125,15 +289,13 @@ public class PlayerInputManager : MonoBehaviour
     {
         isSelecting = false;
 
-        // Hide the placeholders and reveal actual selections after time is up
-        rockImage.gameObject.SetActive(false);
-        paperImage.gameObject.SetActive(false);
-        scissorsImage.gameObject.SetActive(false);
-
-        // Hide placeholders
+        // Hide the placeholders
         rockImagePlaceholder.gameObject.SetActive(false);
         paperImagePlaceholder.gameObject.SetActive(false);
         scissorsImagePlaceholder.gameObject.SetActive(false);
+
+        // Reveal the player's choice immediately when time runs out
+        RevealChoice();
 
         // Trigger the game logic to compare selections
         SpiderGameManager.Instance.CompareSelections();
--- Laro ng Bayan V2/Assets/Scripts/SpiderDerby/SpiderGameManager.cs
+++ Laro ng Bayan V2/Assets/Scripts/SpiderDerby/SpiderGameManager.cs
@@ -1,3 +1,320 @@
+//using UnityEngine;
+//using TMPro;
+//using UnityEngine.UI;
+
+//public class SpiderGameManager : MonoBehaviour
+//{
+//    public static SpiderGameManager Instance;
+
+//    // Player Input Managers
+//    public PlayerInputManager player1Input;
+//    public PlayerInputManager player2Input;
+
+//    // Spider Animation Controllers
+//    public SpiderAnimationController player1Spider;
+//    public SpiderAnimationController player2Spider;
+
+//    // UI Elements
+//    public TextMeshProUGUI resultText;
+//    public TextMeshProUGUI roundText;
+//    public Image player1HealthImage;
+//    public Image player2HealthImage;
+//    public TextMeshProUGUI player1HealthText;
+//    public TextMeshProUGUI player2HealthText;
+//    public GameObject winnerPanel;
+//    public TextMeshProUGUI winnerText;
+//    public TextMeshProUGUI roundWinnerText;
+
+//    // Editable values in Inspector
+//    [Header("Player Settings")]
+//    [SerializeField] private int player1Health = 100;
+//    [SerializeField] private int player2Health = 100;
+//    [SerializeField] private int damagePerAttack = 20;
+//    [SerializeField] private float winnerDelay = 2f;
+//    [SerializeField] private float animationDelay = 0.5f; // Delay for smoother animation timing
+//    [SerializeField] private float deathAnimationDelay = 1.0f; // Delay before playing death animation
+//    [SerializeField] private float drawDelay = 2.0f; // Delay before starting new round after a draw
+
+//    private bool gameOver = false;
+//    private int round = 1;
+//    private float timeToChoose = 5f;
+//    private float timer;
+//    private bool isSelecting = true;
+//    private bool isProcessingRound = false;
+
+//    private void Awake()
+//    {
+//        if (Instance == null) Instance = this;
+//        else Destroy(gameObject);
+//    }
+
+//    void Start()
+//    {
+//        // Set initial health values
+//        player1HealthImage.fillAmount = player1Health / 100f;
+//        player2HealthImage.fillAmount = player2Health / 100f;
+//        player1HealthText.text = "Player 1 Health: " + player1Health;
+//        player2HealthText.text = "Player 2 Health: " + player2Health;
+
+//        winnerPanel.SetActive(false);
+//        timer = timeToChoose;
+
+//        // Ensure spiders are in idle animations to start
+//        if (player1Spider != null) player1Spider.ResetToIdle();
+//        if (player2Spider != null) player2Spider.ResetToIdle();
+//    }
+
+//    void Update()
+//    {
+//        if (!gameOver && isSelecting)
+//        {
+//            if (timer > 0f)
+//            {
+//                timer -= Time.deltaTime;
+//                resultText.text = "Time: " + Mathf.Ceil(timer).ToString();
+//            }
+//            else
+//            {
+//                StopSelecting();
+//            }
+//        }
+
+//        roundText.text = "Round: " + round;
+//    }
+
+//    // Method to compare selections after players make their choice
+//    public void CompareSelections()
+//    {
+//        if (gameOver || isProcessingRound) return;
+
+//        isProcessingRound = true;
+
+//        int player1Choice = player1Input.GetPlayerChoice();
+//        int player2Choice = player2Input.GetPlayerChoice();
+
+//        string result = "It's a draw!";
+
+//        // Compare choices and play appropriate animations with small delay
+//        if (player1Choice == player2Choice)
+//        {
+//            result = "It's a draw!";
+//            // No animations for a draw, spiders remain in idle
+
+//            // FIX: Add a call to start a new round after a short delay
+//            Invoke("StartNewRound", drawDelay);
+//        }
+//        else if ((player1Choice == 1 && player2Choice == 3) || // Rock beats Scissors
+//                 (player1Choice == 2 && player2Choice == 1) || // Paper beats Rock
+//                 (player1Choice == 3 && player2Choice == 2))   // Scissors beats Paper
+//        {
+//            result = "Player 1 Wins Round " + round + "!";
+
+//            // Player 1 attacks
+//            Invoke("PlayPlayer1AttackAnimation", animationDelay);
+
+//            // Player 2 takes damage after a short delay
+//            Invoke("PlayPlayer2DamageAnimation", animationDelay * 2);
+
+//            // Player 2 takes damage
+//            Invoke("Player2TakesDamage", animationDelay * 3);
+//        }
+//        else
+//        {
+//            result = "Player 2 Wins Round " + round + "!";
+
+//            // Player 2 attacks
+//            Invoke("PlayPlayer2AttackAnimation", animationDelay);
+
+//            // Player 1 takes damage after a short delay
+//            Invoke("PlayPlayer1DamageAnimation", animationDelay * 2);
+
+//            // Player 1 takes damage
+//            Invoke("Player1TakesDamage", animationDelay * 3);
+//        }
+
+//        resultText.text = result;
+//        roundWinnerText.text = result;
+//    }
+
+//    // Animation trigger methods
+//    private void PlayPlayer1AttackAnimation()
+//    {
+//        if (player1Spider != null) player1Spider.PlayAttackAnimation();
+//    }
+
+//    private void PlayPlayer2AttackAnimation()
+//    {
+//        if (player2Spider != null) player2Spider.PlayAttackAnimation();
+//    }
+
+//    private void PlayPlayer1DamageAnimation()
+//    {
+//        if (player1Spider != null) player1Spider.PlayDamageTakenAnimation();
+//    }
+
+//    private void PlayPlayer2DamageAnimation()
+//    {
+//        if (player2Spider != null) player2Spider.PlayDamageTakenAnimation();
+//    }
+
+//    private void PlayPlayer1DeathAnimation()
+//    {
+//        if (player1Spider != null) player1Spider.PlayDeathAnimation();
+//    }
+
+//    private void PlayPlayer2DeathAnimation()
+//    {
+//        if (player2Spider != null) player2Spider.PlayDeathAnimation();
+//    }
+
+//    // Damage handlers with animation synchronization
+//    private void Player1TakesDamage()
+//    {
+//        TakeDamage(1);
+//        CheckGameOver();
+//    }
+
+//    private void Player2TakesDamage()
+//    {
+//        TakeDamage(2);
+//        CheckGameOver();
+//    }
+
+//    // Check if game is over
+//    private void CheckGameOver()
+//    {
+//        if (player1Health <= 0)
+//        {
+//            // Play death animation for player 1 spider
+//            Invoke("PlayPlayer1DeathAnimation", deathAnimationDelay);
+
+//            // Show winner panel after death animation has time to play
+//            Invoke("ShowWinnerPanelWithDelay", winnerDelay + deathAnimationDelay);
+//            winnerText.text = "Player 1 Loses the Game!";
+//            gameOver = true;
+//        }
+//        else if (player2Health <= 0)
+//        {
+//            // Play death animation for player 2 spider
+//            Invoke("PlayPlayer2DeathAnimation", deathAnimationDelay);
+
+//            // Show winner panel after death animation has time to play
+//            Invoke("ShowWinnerPanelWithDelay", winnerDelay + deathAnimationDelay);
+//            winnerText.text = "Player 2 Loses the Game!";
+//            gameOver = true;
+//        }
+//        else
+//        {
+//            // Proceed to the next round after animations finish
+//            Invoke("StartNewRound", 3f);  // Increased delay to account for animations
+//        }
+//    }
+
+//    // Method to start a new round
+//    void StartNewRound()
+//    {
+//        if (gameOver) return;
+
+//        // Reset player selections
+//        player1Input.ResetSelection();
+//        player2Input.ResetSelection();
+
+//        // Reset animations to idle
+//        if (player1Spider != null && !player1Spider.IsDead()) player1Spider.ResetToIdle();
+//        if (player2Spider != null && !player2Spider.IsDead()) player2Spider.ResetToIdle();
+
+//        // Reset the timer for the new round
+//        timer = timeToChoose;
+
+//        // Increment round number
+//        round++;
+
+//        // Re-enable player selection
+//        isSelecting = true;
+//        isProcessingRound = false;
+//        player1Input.StartSelecting();
+//        player2Input.StartSelecting();
+//    }
+
+//    // Method to reduce the player's health when they take damage
+//    public void TakeDamage(int player)
+//    {
+//        if (player == 1)
+//        {
+//            player1Health -= damagePerAttack;
+//            player1Health = Mathf.Clamp(player1Health, 0, 100);
+
+//            player1HealthImage.fillAmount = player1Health / 100f;
+//            player1HealthText.text = "Player 1 Health: " + player1Health;
+//        }
+//        else if (player == 2)
+//        {
+//            player2Health -= damagePerAttack;
+//            player2Health = Mathf.Clamp(player2Health, 0, 100);
+
+//            player2HealthImage.fillAmount = player2Health / 100f;
+//            player2HealthText.text = "Player 2 Health: " + player2Health;
+//        }
+//    }
+
+//    // Stop the player's ability to select after the time runs out
+//    public void StopSelecting()
+//    {
+//        if (!isSelecting) return;
+
+//        isSelecting = false;
+//        player1Input.StopSelecting();
+//        player2Input.StopSelecting();
+
+//        // Compare selections after both players have made their choices
+//        CompareSelections();
+//    }
+
+//    // Method to show the winner panel with a delay
+//    private void ShowWinnerPanelWithDelay()
+//    {
+//        winnerPanel.SetActive(true);
+//    }
+
+//    // Reset the entire game (call this from a button click)
+//    public void ResetGame()
+//    {
+//        // Reset health
+//        player1Health = 100;
+//        player2Health = 100;
+
+//        // Reset UI
+//        player1HealthImage.fillAmount = player1Health / 100f;
+//        player2HealthImage.fillAmount = player2Health / 100f;
+//        player1HealthText.text = "Player 1 Health: " + player1Health;
+//        player2HealthText.text = "Player 2 Health: " + player2Health;
+
+//        // Reset round
+//        round = 1;
+
+//        // Reset game state
+//        gameOver = false;
+//        isProcessingRound = false;
+
+//        // Reset animations
+//        if (player1Spider != null) player1Spider.ResetToIdle();
+//        if (player2Spider != null) player2Spider.ResetToIdle();
+
+//        // Hide winner panel
+//        winnerPanel.SetActive(false);
+
+//        // Reset player selections
+//        player1Input.ResetSelection();
+//        player2Input.ResetSelection();
+
+//        // Start new round
+//        timer = timeToChoose;
+//        isSelecting = true;
+//        player1Input.StartSelecting();
+//        player2Input.StartSelecting();
+//    }
+//}
+
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -59,6 +376,10 @@ public class SpiderGameManager : MonoBehaviour
         winnerPanel.SetActive(false);
         timer = timeToChoose;
 
+        // Hide result text at the start
+        resultText.gameObject.SetActive(false);
+        roundWinnerText.gameObject.SetActive(false);
+
         // Ensure spiders are in idle animations to start
         if (player1Spider != null) player1Spider.ResetToIdle();
         if (player2Spider != null) player2Spider.ResetToIdle();
@@ -71,6 +392,8 @@ public class SpiderGameManager : MonoBehaviour
             if (timer > 0f)
             {
                 timer -= Time.deltaTime;
+                // Only show time remaining during selection phase
+                resultText.gameObject.SetActive(true);
                 resultText.text = "Time: " + Mathf.Ceil(timer).ToString();
             }
             else
@@ -89,8 +412,14 @@ public class SpiderGameManager : MonoBehaviour
 
         isProcessingRound = true;
 
+        // Hide the time display text
+        resultText.gameObject.SetActive(false);
+
         int player1Choice = player1Input.GetPlayerChoice();
         int player2Choice = player2Input.GetPlayerChoice();
+
+        // Note: The choices are now revealed when StopSelecting() is called
+        // so no need to call RevealChoice() here
 
         string result = "It's a draw!";
 
@@ -99,6 +428,9 @@ public class SpiderGameManager : MonoBehaviour
         {
             result = "It's a draw!";
             // No animations for a draw, spiders remain in idle
+
+            // Show result text after a short delay
+            Invoke("ShowResultText", animationDelay);
 
             // FIX: Add a call to start a new round after a short delay
             Invoke("StartNewRound", drawDelay);
@@ -117,6 +449,9 @@ public class SpiderGameManager : MonoBehaviour
 
             // Player 2 takes damage
             Invoke("Player2TakesDamage", animationDelay * 3);
+
+            // Show result text after animations start
+            Invoke("ShowResultText", animationDelay * 2);
         }
         else
         {
@@ -130,10 +465,21 @@ public class SpiderGameManager : MonoBehaviour
 
             // Player 1 takes damage
             Invoke("Player1TakesDamage", animationDelay * 3);
+
+            // Show result text after animations start
+            Invoke("ShowResultText", animationDelay * 2);
         }
 
+        // Store the result but don't show it yet
         resultText.text = result;
         roundWinnerText.text = result;
+    }
+
+    // Method to show result text at the appropriate time
+    private void ShowResultText()
+    {
+        resultText.gameObject.SetActive(true);
+        roundWinnerText.gameObject.SetActive(true);
     }
 
     // Animation trigger methods
@@ -219,6 +565,10 @@ public class SpiderGameManager : MonoBehaviour
         player1Input.ResetSelection();
         player2Input.ResetSelection();
 
+        // Hide result texts for new round
+        resultText.gameObject.SetActive(false);
+        roundWinnerText.gameObject.SetActive(false);
+
         // Reset animations to idle
         if (player1Spider != null && !player1Spider.IsDead()) player1Spider.ResetToIdle();
         if (player2Spider != null && !player2Spider.IsDead()) player2Spider.ResetToIdle();
@@ -288,6 +638,10 @@ public class SpiderGameManager : MonoBehaviour
         player2HealthImage.fillAmount = player2Health / 100f;
         player1HealthText.text = "Player 1 Health: " + player1Health;
         player2HealthText.text = "Player 2 Health: " + player2Health;
+
+        // Hide result texts
+        resultText.gameObject.SetActive(false);
+        roundWinnerText.gameObject.SetActive(false);
 
         // Reset round
         round = 1;
