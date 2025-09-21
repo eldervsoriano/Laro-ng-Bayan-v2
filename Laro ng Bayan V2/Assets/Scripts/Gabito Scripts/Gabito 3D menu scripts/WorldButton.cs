9f53c9ec76806f005723f9093cc06d7a3f5bd619
Christian <christiangabito12@gmail.com>
2025-09-22 03:27:55 +0800

    Overhaul for 3D Menu
    Added new assets more for 3d menu

--- Laro ng Bayan V2/Assets/Scripts/Gabito Scripts/Gabito 3D menu scripts/WorldButton.cs
+++ Laro ng Bayan V2/Assets/Scripts/Gabito Scripts/Gabito 3D menu scripts/WorldButton.cs
@@ -4,38 +4,75 @@ using UnityEngine;
 
 public class WorldButton : MonoBehaviour
 {
+    private Renderer rend;
+    private Color defaultColor;
+
+    // ORIGINALLY USED FOR CAMERA BUTTON
+
     // Assign this in inspector if you want each button to call different methods
-    public string buttonName;
+    //public string buttonName;
 
-    private void OnMouseDown()
+    //private void OnMouseDown()
+    //{
+    //    // This is called automatically when the object is clicked
+    //    Debug.Log(buttonName + " was clicked!");
+
+    //    // Example: switch camera when clicked
+    //    if (buttonName == "Play")
+    //    {
+    //        FindObjectOfType<UITransitionManager>().GoToPlay();
+    //    }
+    //    else if (buttonName == "Options")
+    //    {
+    //        FindObjectOfType<UITransitionManager>().GoToOptions();
+    //    }
+
+    //    else if (buttonName == "Credits")
+    //    {
+    //        FindObjectOfType<UITransitionManager>().GoToCredits();
+    //    }
+    //}
+
+    private void Start()
     {
-        // This is called automatically when the object is clicked
-        Debug.Log(buttonName + " was clicked!");
-
-        // Example: switch camera when clicked
-        if (buttonName == "Play")
+        rend = GetComponent<Renderer>();
+        if (rend != null)
         {
-            FindObjectOfType<UITransitionManager>().GoToPlay();
-        }
-        else if (buttonName == "Options")
-        {
-            FindObjectOfType<UITransitionManager>().GoToOptions();
-        }
-
-        else if (buttonName == "Credits")
-        {
-            FindObjectOfType<UITransitionManager>().GoToCredits();
+            defaultColor = rend.material.color; // save original
         }
     }
 
+    private void OnEnable()
+    {
+        if (rend != null)
+            rend.material.color = defaultColor; // reseter color after going back from jumping to each panels
+    }
+
+
+
     private void OnMouseEnter()
     {
-        GetComponent<Renderer>().material.color = Color.yellow;
+        if (rend != null)
+            rend.material.color = defaultColor * 0.8f; // black (0.8 means 80% opacity), hover
+                                                      
+    }
+
+    private void OnMouseDown()
+    {
+        if (rend != null)
+            rend.material.color = defaultColor * 0.7f; // pressed
+    }
+
+    private void OnMouseUp()
+    {
+        if (rend != null)
+            rend.material.color = defaultColor * 0.8f; // return to hover color
     }
 
     private void OnMouseExit()
     {
-        GetComponent<Renderer>().material.color = Color.white;
+        if (rend != null) 
+            rend.material.color = defaultColor; // back to normal
     }
 
 }
